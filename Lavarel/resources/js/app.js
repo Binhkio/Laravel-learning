772502a3ee222095c07c4cee9477294b138035ab
Binhkio <quan.va205115@gmail.com>
2022-05-05 23:05:29 +0700

    new js func

--- Lavarel/resources/js/app.js
+++ Lavarel/resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+let x = [1, 2, 3, 4]
+
+Array.prototype.map2 = function(func) {
+    let result = []
+    console.log(this)
+}
+
+console.log(x.map2((pt, i)=>{pt*2}));
