99c3b9e9d9cf4a95df47e8ff3e1710ff1b84de8e
Binhkio <quan.va205115@gmail.com>
2022-05-15 23:40:45 +0700

    nothing change

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,27 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import {Routes, Route, Navigate} from 'react-router-dom';
-import Login from './components/Login'
-import Register from './components/Register'
-import Auth from './components/Auth'
-import AuthContextProvider from './contexts/AuthContext'
-
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
-  return (
-      <div>
-      <h1>tran ngoc</h1>
-      
-      {/* // <Routes>
-      //   <Route path ='/' element ={<Auth/>} >
-      //     <Route path='/' element={<Navigate to="/login" />} />
-      //     <Route path='/login' element={<Login />}/>
-      //     <Route path='/register' element={<Register />}/>
-      //   </Route>
-      // </Routes> */}
-      
-    </div>
-  )
 }
 
 export default App;
--- reactjs/src/contexts/constants.js
+++
@@ -1,3 +0,0 @@
-// export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://localhost:3000/api' : 'someurl'
-
-// export const LOCAL_STORAGE_TOKEN_NAME = 'tran-ngoc'
